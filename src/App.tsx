753965fa7229c612166b2f7620e1091e64fb4205
Chino <chinomsoaugustine073@gmail.com>
2025-09-14 19:41:29 -0700

    Debugging

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from "./components/Home/Home.js"
-import Navbar from './components/Home/Navbar.js'
-import About from './components/Aboutme/About.js'
-import ContactForm from './components/Home/contactForm.js';
+import Home from "./components/Home/Home.js";
+import Navbar from "./components/Home/Navbar.js";
+import About from "./components/Aboutme/About.js";
+import ContactForm from "./components/Home/ContactForm.js";
 
 function App() {
   return (
     <Router>
-     <Navbar />
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element = {<About />} />
-        <Route path='/contactForm' element={<ContactForm />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contactform" element={<ContactForm />} />
       </Routes>
     </Router>
   );

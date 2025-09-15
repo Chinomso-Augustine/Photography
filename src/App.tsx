1811d79dd079be097c3984efbfa6a48155ef7ada
Chino <chinomsoaugustine073@gmail.com>
2025-09-14 19:53:05 -0700

    Db

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from "./components/Home/Home";
+import Home from "./components/Home/home";
 import Navbar from "./components/Home/Navbar";
 import About from "./components/Aboutme/About";
-import ContactForm from "./components/Home/ContactForm";
+import ContactForm from "./components/Home/contactForm";
 
 function App() {
   return (
